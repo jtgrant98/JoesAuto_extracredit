8b852449f2533db822739a66e0d87eb1a2dac576
Josh Grant <Josh_grant1@baylor.edu>
2021-10-24 14:07:33 -0500

    more changes to formatting

--- Joes_auto_program.py
+++ Joes_auto_program.py
@@ -1,31 +1,31 @@
-import Joes_auto_class as sc
+import Joes_auto_class as xx
 
 
 print('CUSTOMER DETAILS')
 name = input('Enter name : ')
 address = input('Enter address : ')
 phone = input('Enter phone : ')
-customer = sc.Customer(name, address, phone)
+customer = xx.Customer(name, address, phone)
 print()
 print('CAR DETAILS')
 make = input('Enter manufacturer : ')
 model = input('Enter car model : ')
 year = int(input('Enter year of manufacture : '))
-car = sc.Car(make, model, year)
+car = xx.Car(make, model, year)
 print()
 print('CHARGE DETAILS')
 pcharge = float(input('Enter charge amount for parts : '))
 lcharge = float(input('Enter charge amount for labor : '))
-charges = sc.ServiceQuote(pcharge, lcharge)
+charges = xx.ServiceQuote(pcharge, lcharge)
 print()
-print('\nCustomer Information')
+print('\nCUSTOMER INFORMATION')
 print(f'Name : {customer.get_name()}\n'
       f'CONTACT INFO : {customer.get_address()}  ({customer.get_phone()})')
-print('\nCar Information')
+print('\nCAR INFORMATION')
 print(f'Manufacturer = {car.get_make()}\n'
       f'Model : {car.get_model()}\nYear : {car.get_year()}')
-print('\nCharges Information')
-print(f'Parts Charges : {charges.get_parts_charges()}\n'
-      f'Labour Charges : {charges.get_labour_charges()}\n'
+print('\nCHARGE INFORMATION')
+print(f'Parts cost : {charges.get_parts_charges()}\n'
+      f'Labour cost : {charges.get_labour_charges()}\n'
       f'Sales Tax : {charges.get_sales_tax()}\n'
       f'Total Charges : {charges.get_total_charges()}')
